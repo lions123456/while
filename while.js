c1f82f191f88e49dcf3a2b1b844ed1e493e35f45
lions123456 <citroncitruslex@gmail.com>
2024-08-14 13:31:22 +0400

    first

--- while.js
+++ while.js
@@ -1,1 +1,13 @@
-gi
+let num = 50;
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+// пока num == 50 цикл работает
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+// сначала сделаем, потом проверяем условие
