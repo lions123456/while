73907a6403579b5804b87e1fbc0ca11bdf352812
lions123456 <155250809+lions123456@users.noreply.github.com>
2024-08-20 20:55:04 +0400

    Update while.js

--- while.js
+++ while.js
@@ -11,3 +11,27 @@ do {
 }
 while (num < 55);
 // сначала сделаем, потом проверяем условие
+
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+// перебор через for
+
+
+for (let i = 1; i < 10; i++) {
+    if (i == 6) {
+        break
+    }
+    console.log(i);
+}
+// стоп через break
+
+
+for (let i = 1; i < 10; i++) {
+    if (i == 6) {
+        continue
+    }
+    console.log(i);
+}
+// for continue
